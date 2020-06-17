146743a86965a1e9c693a808f50d6ed08a6a5909
Ken Cheung <wasd0109.dev@gmail.com>
2020-06-17 17:59:55 +0800

    recolor, add articles, added function

---
+++ keyaki.js
@@ -0,0 +1,298 @@
+const keyaki = [
+  {
+    name: "石森虹花",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/307/bc6cc498c899e28fa0c5c81914a66/400_320_102400.jpg",
+    birthday: "1997,5,7",
+    birthplace: "宮城県",
+    gen: "1期",
+  },
+  {
+    name: "上村莉菜",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/324/1f4df8856787a2f71181a54e93d1b/400_320_102400.jpg",
+    birthday: "1997,1,4",
+    birthplace: "千葉県",
+    gen: "1期",
+  },
+  {
+    name: "尾関梨香",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/8af/cee89703226c930a556b09103b7dc/400_320_102400.jpg",
+    birthday: "1997,10,7",
+    birthplace: "神奈川県",
+    gen: "1期",
+  },
+  {
+    name: "小池美波",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/7c6/04426f2edf38d0a5d061c44e41206/400_320_102400.jpg",
+    birthday: "1998,11,14",
+    birthplace: "兵庫県",
+    gen: "1期",
+  },
+  {
+    name: "小林由依",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/7cf/798fa6b27706afe92fd050e3340b0/400_320_102400.jpg",
+    birthday: "1999,10,23",
+    birthplace: "埼玉県",
+    gen: "1期",
+  },
+  {
+    name: "齋藤冬優花",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/ff9/fbacd7c8b008e6f7438b18b9e1703/400_320_102400.jpg",
+    birthday: "1998,2,15",
+    birthplace: "東京都",
+    gen: "1期",
+  },
+  {
+    name: "佐藤詩織",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/b79/07a66850d238e5b177191135780c7/400_320_102400.jpg",
+    birthday: "1996,11,16",
+    birthplace: "東京都",
+    gen: "1期",
+  },
+  {
+    name: "菅井友香",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/437/355878101ad2e68158dad9e21d996/400_320_102400.jpg",
+    birthday: "1995,11,29",
+    birthplace: "東京都",
+    gen: "1期",
+  },
+  {
+    name: "土生瑞穂",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/7db/1e7ec381376e5bb41b8817f877e3e/400_320_102400.png",
+    birthday: "1997,7,7",
+    birthplace: "東京都",
+    gen: "1期",
+  },
+  {
+    name: "原田葵",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/a08/c494dfca338ddfec383e28e115226/400_320_102400.jpg",
+    birthday: "2000,5,7",
+    birthplace: "東京都",
+    gen: "1期",
+  },
+  {
+    name: "守屋茜",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/103/4f2a17f7f544a1635c244502dc8ea/400_320_102400.jpg",
+    birthday: "1997,11,12",
+    birthplace: "宮城県",
+    gen: "1期",
+  },
+  {
+    name: "渡辺梨加",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/a64/2be75bedbbe36e81d9811eae8b300/400_320_102400.jpg",
+    birthday: "1995,5,16",
+    birthplace: "茨城県",
+    gen: "1期",
+  },
+  {
+    name: "渡邉理佐",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/ade/bf8fc87063fee788309b156cf6da3/400_320_102400.png",
+    birthday: "1998,7,27",
+    birthplace: "茨城県",
+    gen: "1期",
+  },
+  {
+    name: "井上梨名",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/96f/4c12e90d08ca23df0099faf600c11/400_320_102400.jpg",
+    birthday: "2001,1,29",
+    birthplace: "兵庫県",
+    gen: "2期",
+  },
+  {
+    name: "関有美子",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/8c8/8e1aea5febafd0d9b8da3577c8ec3/400_320_102400.jpg",
+    birthday: "1998,6,29",
+    birthplace: "福岡県",
+    gen: "2期",
+  },
+  {
+    name: "武元唯衣",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/2c3/424aa68856092e236e64cf5e6a51f/400_320_102400.jpg",
+    birthday: "2002,3,23",
+    birthplace: "滋賀県",
+    gen: "2期",
+  },
+  {
+    name: "田村保乃",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/de3/3be545df210539c7271750dbf592c/400_320_102400.jpg",
+    birthday: "1998,10,21",
+    birthplace: "大阪府",
+    gen: "2期",
+  },
+  {
+    name: "藤吉夏鈴",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/ad6/8083ce0da0ce07f0773b0457b02ee/400_320_102400.jpg",
+    birthday: "2001,8,29",
+    birthplace: "大阪府",
+    gen: "2期",
+  },
+  {
+    name: "松田里奈",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/003/4e69043115569ecd5ca36e185adbe/400_320_102400.jpg",
+    birthday: "1999,10,13",
+    birthplace: "宮崎県",
+    gen: "2期",
+  },
+  {
+    name: "松平璃子",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/9fd/082644b648af3a9975bd7e2febc76/400_320_102400.jpg",
+    birthday: "1998,5,5",
+    birthplace: "東京都",
+    gen: "2期",
+  },
+  {
+    name: "森田ひかる",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/001/683b42b90da66266ca4e4955c4265/400_320_102400.jpg",
+    birthday: "2001,7,10",
+    birthplace: "福岡県",
+    gen: "2期",
+  },
+  {
+    name: "山﨑天",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/eb2/c26d6b1bc38b36ea6ab479ab93e87/400_320_102400.jpg",
+    birthday: "2005,9,28",
+    birthplace: "大阪府",
+    gen: "2期",
+  },
+  {
+    name: "遠藤光莉",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/e94/01b56ff4808462567b926553f7a95/400_320_102400.jpg",
+    birthday: "1999,4,17",
+    birthplace: "神奈川県",
+    gen: "2期",
+  },
+  {
+    name: "大園玲",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/ba7/1d5ff2207e9603ea92250d8ad2303/400_320_102400.jpg",
+    birthday: "2000,4,18",
+    birthplace: "鹿児島県",
+    gen: "2期",
+  },
+  {
+    name: "大沼晶保",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/386/124345c2dc53938c7b7853265d5de/400_320_102400.jpg",
+    birthday: "1999,10,12",
+    birthplace: "静岡県",
+    gen: "2期",
+  },
+  {
+    name: "幸阪茉里乃",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/7b3/89d29b7c31500ed7e0fec450a1e51/400_320_102400.jpg",
+    birthday: "2002,12,19",
+    birthplace: "三重県",
+    gen: "2期",
+  },
+  {
+    name: "増本綺良",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/686/0ee5528484eba27630877fd6bf7f5/400_320_102400.jpg",
+    birthday: "2002,1,12",
+    birthplace: "兵庫県",
+    gen: "2期",
+  },
+  {
+    name: "守屋麗奈",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/1f9/96c91f97d6a329fadfd38e6c105e8/400_320_102400.jpg",
+    birthday: "2000,1,2",
+    birthplace: "東京都",
+    gen: "2期",
+  },
+  {
+    name: "原田まゆ",
+    img:
+      "https://48pedia.org/images/3/3b/2015%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E5%8E%9F%E7%94%B0%E3%81%BE%E3%82%86_1.png",
+    birthday: "1998,5,2",
+    birthplace: "東京都",
+    gen: "1期(卒業)",
+  },
+  {
+    name: "今泉佑唯",
+    img:
+      "https://48pedia.org/images/c/ce/2018%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E4%BB%8A%E6%B3%89%E4%BD%91%E5%94%AF_2.jpg",
+    birthday: "1998,9,30",
+    birthplace: "神奈川県",
+    gen: "1期(卒業)",
+  },
+  {
+    name: "志田愛佳",
+    img:
+      "https://48pedia.org/images/thumb/7/7c/2018%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E5%BF%97%E7%94%B0%E6%84%9B%E4%BD%B3.jpg/800px-2018%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E5%BF%97%E7%94%B0%E6%84%9B%E4%BD%B3.jpg",
+    birthday: "1998,11,23",
+    birthplace: "新潟県",
+    gen: "1期(卒業)",
+  },
+  {
+    name: "米谷奈々未",
+    img:
+      "https://48pedia.org/images/1/1f/2018%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E7%B1%B3%E8%B0%B7%E5%A5%88%E3%80%85%E6%9C%AA_2.jpg",
+    birthday: "2000,2,24",
+    birthplace: "大阪府",
+    gen: "1期(卒業)",
+  },
+  {
+    name: "長濱ねる",
+    img:
+      "https://48pedia.org/images/5/5d/2019%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E9%95%B7%E6%BF%B1%E3%81%AD%E3%82%8B.jpg",
+    birthday: "1998,9,4",
+    birthplace: "長崎県",
+    gen: "1.5期(卒業)",
+  },
+  {
+    name: "織田奈那",
+    img:
+      "https://cdn.keyakizaka46.com/images/14/7c6/04426f2edf38d0a5d061c44e41206/400_320_102400.jpg",
+    birthday: "1998,6,4",
+    birthplace: "静岡県",
+    gen: "1期(卒業)",
+  },
+  {
+    name: "鈴本美愉",
+    img:
+      "https://48pedia.org/images/e/e8/2019%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E9%88%B4%E6%9C%AC%E7%BE%8E%E6%84%89.jpg",
+    birthday: "1997,12,5",
+    birthplace: "愛知県",
+    gen: "1期(卒業)",
+  },
+  {
+    name: "平手友梨奈",
+    img:
+      "https://48pedia.org/images/1/1e/2019%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E5%B9%B3%E6%89%8B%E5%8F%8B%E6%A2%A8%E5%A5%88.jpg",
+    birthday: "2001,6,25",
+    birthplace: "愛知県",
+    gen: "1期(卒業)",
+  },
+  {
+    name: "長沢菜々香",
+    img:
+      "https://48pedia.org/images/4/44/2020%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E9%95%B7%E6%B2%A2%E8%8F%9C%E3%80%85%E9%A6%99.jpg",
+    birthday: "1997,4,23",
+    birthplace: "山形県",
+    gen: "1期(卒業)",
+  },
+];
---
+++ site.js
@@ -0,0 +1,60 @@
+const navBtn = document.querySelector("#navBtn");
+const navContent = document.querySelector("#navContent");
+const date = document.querySelector("#date");
+const infoBox = document.querySelector("#infoBox");
+
+const list = keyaki;
+
+const openNavContent = () => {
+  navContent.classList.toggle("hidden");
+};
+
+navBtn.addEventListener("click", openNavContent);
+
+const getToday = () => {
+  let today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  const yyyy = today.getFullYear();
+  today = `${yyyy}/${mm}/${dd}`;
+  date.textContent = today;
+};
+
+const getTodayDate = () => {
+  let todayDate = new Date();
+  const dd = String(todayDate.getDate());
+  const mm = String(todayDate.getMonth() + 1); //January is 0!
+  todayDate = `${mm},${dd}`;
+  return todayDate;
+};
+
+// const today = getTodayDate();
+
+const today = "6,25";
+
+const calculateAge = (birthday) => {
+  var diff_ms = Date.now() - birthday.getTime();
+  var age_dt = new Date(diff_ms);
+  return Math.abs(age_dt.getUTCFullYear() - 1970);
+};
+
+const findBirthdayMember = (list) => {
+  const birthdayList = list.filter((member) => member.birthday.includes(today));
+  return birthdayList;
+};
+
+const generateBirthday = (birthdayMember) => {
+  if (birthdayMember.length === 0) return;
+  birthdayMember.forEach((member) => {
+    const { name, birthday } = member;
+    const age = calculateAge(new Date(birthday));
+    const paragraph = document.createElement("p");
+    paragraph.textContent = `Happy birthday ${name}, age ${age}`;
+    infoBox.appendChild(paragraph);
+  });
+};
+
+const checkBirthday = (list) => generateBirthday(findBirthdayMember(list));
+
+getToday();
+checkBirthday(list);
