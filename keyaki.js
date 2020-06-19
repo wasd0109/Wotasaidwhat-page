192b5ea3ceff391a4641a02ab744295ca969a196
Ken Cheung <wasd0109.dev@gmail.com>
2020-06-20 00:17:05 +0800

    Fix Safari date issue and non-standard date format from list
    Safari does not support the original code for age calculation,
    moment.js is used to replace traditional JS Date for ease of usage,
    yet the original birthday format on member lists are non-standard and
    therefore changed to YYYY-mm-dd format to prevent lost of support
    from future moment.js release.

--- keyaki.js
+++ keyaki.js
@@ -4,7 +4,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/307/bc6cc498c899e28fa0c5c81914a66/400_320_102400.jpg",
-    birthday: "1997,5,7",
+    birthday: "1997-05-7",
     birthplace: "宮城県",
     gen: "1期",
   },
@@ -13,7 +13,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/324/1f4df8856787a2f71181a54e93d1b/400_320_102400.jpg",
-    birthday: "1997,1,4",
+    birthday: "1997-01-04",
     birthplace: "千葉県",
     gen: "1期",
   },
@@ -22,7 +22,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/8af/cee89703226c930a556b09103b7dc/400_320_102400.jpg",
-    birthday: "1997,10,7",
+    birthday: "1997-10-07",
     birthplace: "神奈川県",
     gen: "1期",
   },
@@ -31,7 +31,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/7c6/04426f2edf38d0a5d061c44e41206/400_320_102400.jpg",
-    birthday: "1998,11,14",
+    birthday: "1998-11-14",
     birthplace: "兵庫県",
     gen: "1期",
   },
@@ -40,7 +40,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/7cf/798fa6b27706afe92fd050e3340b0/400_320_102400.jpg",
-    birthday: "1999,10,23",
+    birthday: "1999-10-23",
     birthplace: "埼玉県",
     gen: "1期",
   },
@@ -49,7 +49,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/ff9/fbacd7c8b008e6f7438b18b9e1703/400_320_102400.jpg",
-    birthday: "1998,2,15",
+    birthday: "1998-02-15",
     birthplace: "東京都",
     gen: "1期",
   },
@@ -58,7 +58,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/b79/07a66850d238e5b177191135780c7/400_320_102400.jpg",
-    birthday: "1996,11,16",
+    birthday: "1996-11-16",
     birthplace: "東京都",
     gen: "1期",
   },
@@ -67,7 +67,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/437/355878101ad2e68158dad9e21d996/400_320_102400.jpg",
-    birthday: "1995,11,29",
+    birthday: "1995-11-29",
     birthplace: "東京都",
     gen: "1期",
   },
@@ -76,7 +76,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/7db/1e7ec381376e5bb41b8817f877e3e/400_320_102400.png",
-    birthday: "1997,7,7",
+    birthday: "1997-07-07",
     birthplace: "東京都",
     gen: "1期",
   },
@@ -85,7 +85,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/a08/c494dfca338ddfec383e28e115226/400_320_102400.jpg",
-    birthday: "2000,5,7",
+    birthday: "2000-05-07",
     birthplace: "東京都",
     gen: "1期",
   },
@@ -94,7 +94,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/103/4f2a17f7f544a1635c244502dc8ea/400_320_102400.jpg",
-    birthday: "1997,11,12",
+    birthday: "1997-11-12",
     birthplace: "宮城県",
     gen: "1期",
   },
@@ -103,7 +103,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/a64/2be75bedbbe36e81d9811eae8b300/400_320_102400.jpg",
-    birthday: "1995,5,16",
+    birthday: "1995-05-16",
     birthplace: "茨城県",
     gen: "1期",
   },
@@ -112,7 +112,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/ade/bf8fc87063fee788309b156cf6da3/400_320_102400.png",
-    birthday: "1998,7,27",
+    birthday: "1998-07-27",
     birthplace: "茨城県",
     gen: "1期",
   },
@@ -121,7 +121,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/96f/4c12e90d08ca23df0099faf600c11/400_320_102400.jpg",
-    birthday: "2001,1,29",
+    birthday: "2001-01-29",
     birthplace: "兵庫県",
     gen: "2期",
   },
@@ -130,7 +130,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/8c8/8e1aea5febafd0d9b8da3577c8ec3/400_320_102400.jpg",
-    birthday: "1998,6,29",
+    birthday: "1998-06-29",
     birthplace: "福岡県",
     gen: "2期",
   },
@@ -139,7 +139,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/2c3/424aa68856092e236e64cf5e6a51f/400_320_102400.jpg",
-    birthday: "2002,3,23",
+    birthday: "2002-03-23",
     birthplace: "滋賀県",
     gen: "2期",
   },
@@ -148,7 +148,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/de3/3be545df210539c7271750dbf592c/400_320_102400.jpg",
-    birthday: "1998,10,21",
+    birthday: "1998-10-21",
     birthplace: "大阪府",
     gen: "2期",
   },
@@ -157,7 +157,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/ad6/8083ce0da0ce07f0773b0457b02ee/400_320_102400.jpg",
-    birthday: "2001,8,29",
+    birthday: "2001-08-29",
     birthplace: "大阪府",
     gen: "2期",
   },
@@ -166,7 +166,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/003/4e69043115569ecd5ca36e185adbe/400_320_102400.jpg",
-    birthday: "1999,10,13",
+    birthday: "1999-10-13",
     birthplace: "宮崎県",
     gen: "2期",
   },
@@ -175,7 +175,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/9fd/082644b648af3a9975bd7e2febc76/400_320_102400.jpg",
-    birthday: "1998,5,5",
+    birthday: "1998-05-05",
     birthplace: "東京都",
     gen: "2期",
   },
@@ -184,7 +184,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/001/683b42b90da66266ca4e4955c4265/400_320_102400.jpg",
-    birthday: "2001,7,10",
+    birthday: "2001-07-10",
     birthplace: "福岡県",
     gen: "2期",
   },
@@ -193,7 +193,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/eb2/c26d6b1bc38b36ea6ab479ab93e87/400_320_102400.jpg",
-    birthday: "2005,9,28",
+    birthday: "2005-09-28",
     birthplace: "大阪府",
     gen: "2期",
   },
@@ -202,7 +202,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/e94/01b56ff4808462567b926553f7a95/400_320_102400.jpg",
-    birthday: "1999,4,17",
+    birthday: "1999-04-17",
     birthplace: "神奈川県",
     gen: "2期",
   },
@@ -211,7 +211,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/ba7/1d5ff2207e9603ea92250d8ad2303/400_320_102400.jpg",
-    birthday: "2000,4,18",
+    birthday: "2000-04-18",
     birthplace: "鹿児島県",
     gen: "2期",
   },
@@ -220,7 +220,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/386/124345c2dc53938c7b7853265d5de/400_320_102400.jpg",
-    birthday: "1999,10,12",
+    birthday: "1999-10-12",
     birthplace: "静岡県",
     gen: "2期",
   },
@@ -229,7 +229,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/7b3/89d29b7c31500ed7e0fec450a1e51/400_320_102400.jpg",
-    birthday: "2002,12,19",
+    birthday: "2002-12-19",
     birthplace: "三重県",
     gen: "2期",
   },
@@ -238,7 +238,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/686/0ee5528484eba27630877fd6bf7f5/400_320_102400.jpg",
-    birthday: "2002,1,12",
+    birthday: "2002-01-12",
     birthplace: "兵庫県",
     gen: "2期",
   },
@@ -247,7 +247,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/1f9/96c91f97d6a329fadfd38e6c105e8/400_320_102400.jpg",
-    birthday: "2000,1,2",
+    birthday: "2000-01-2",
     birthplace: "東京都",
     gen: "2期",
   },
@@ -256,7 +256,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://48pedia.org/images/3/3b/2015%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E5%8E%9F%E7%94%B0%E3%81%BE%E3%82%86_1.png",
-    birthday: "1998,5,2",
+    birthday: "1998-05-2",
     birthplace: "東京都",
     gen: "1期(卒業)",
   },
@@ -265,7 +265,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://48pedia.org/images/c/ce/2018%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E4%BB%8A%E6%B3%89%E4%BD%91%E5%94%AF_2.jpg",
-    birthday: "1998,9,30",
+    birthday: "1998-9-30",
     birthplace: "神奈川県",
     gen: "1期(卒業)",
   },
@@ -274,7 +274,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://48pedia.org/images/thumb/7/7c/2018%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E5%BF%97%E7%94%B0%E6%84%9B%E4%BD%B3.jpg/800px-2018%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E5%BF%97%E7%94%B0%E6%84%9B%E4%BD%B3.jpg",
-    birthday: "1998,11,23",
+    birthday: "1998-11-23",
     birthplace: "新潟県",
     gen: "1期(卒業)",
   },
@@ -283,7 +283,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://48pedia.org/images/1/1f/2018%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E7%B1%B3%E8%B0%B7%E5%A5%88%E3%80%85%E6%9C%AA_2.jpg",
-    birthday: "2000,2,24",
+    birthday: "2000-02-24",
     birthplace: "大阪府",
     gen: "1期(卒業)",
   },
@@ -292,7 +292,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://48pedia.org/images/5/5d/2019%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E9%95%B7%E6%BF%B1%E3%81%AD%E3%82%8B.jpg",
-    birthday: "1998,9,4",
+    birthday: "1998-09-04",
     birthplace: "長崎県",
     gen: "1.5期(卒業)",
   },
@@ -301,7 +301,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://cdn.keyakizaka46.com/images/14/7c6/04426f2edf38d0a5d061c44e41206/400_320_102400.jpg",
-    birthday: "1998,6,4",
+    birthday: "1998-06-04",
     birthplace: "静岡県",
     gen: "1期(卒業)",
   },
@@ -310,7 +310,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://48pedia.org/images/e/e8/2019%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E9%88%B4%E6%9C%AC%E7%BE%8E%E6%84%89.jpg",
-    birthday: "1997,12,5",
+    birthday: "1997-12-05",
     birthplace: "愛知県",
     gen: "1期(卒業)",
   },
@@ -319,7 +319,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://48pedia.org/images/1/1e/2019%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E5%B9%B3%E6%89%8B%E5%8F%8B%E6%A2%A8%E5%A5%88.jpg",
-    birthday: "2001,6,25",
+    birthday: "2001-06-25",
     birthplace: "愛知県",
     gen: "1期(卒業)",
   },
@@ -328,7 +328,7 @@ const keyaki = [
     group: "欅坂46",
     img:
       "https://48pedia.org/images/4/44/2020%E5%B9%B4%E6%AC%85%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E9%95%B7%E6%B2%A2%E8%8F%9C%E3%80%85%E9%A6%99.jpg",
-    birthday: "1997,4,23",
+    birthday: "1997-04-23",
     birthplace: "山形県",
     gen: "1期(卒業)",
   },
